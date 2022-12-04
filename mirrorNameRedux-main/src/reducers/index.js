ee0750c3ecf030cfb3b723ab7d1d9b415e0bf7d5
Utsav <utsav96jaiswal@gmail.com>
2022-12-04 20:09:21 +0530

    project update

--- mirrorNameRedux-main/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import changeName from "./nameMirror";
-
-import { combineReducers } from "redux";
-
-const rootReducer = combineReducers ({
-    changeName
-})
-
-export default rootReducer;
--- mirror_with_tailwind_react-master/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-function Button(props){
-    return(
-        <div className={props.styles}>
-            <button className={props.myBorder} onClick={props.myClick} >{props.name}</button>
-        </div>
-    )
-}
-
-export default Button;
